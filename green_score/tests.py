795bc9080d010aa3dc41a9ab2be1e0850643ab5d
Jatin <jatinjindal1199@gmail.com>
2023-06-30 21:04:23 +0530

    Ci Pipeline to run tests

--- green_score/tests.py
+++ green_score/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_post_model(self):
+        self.assertEqual(1,1)
